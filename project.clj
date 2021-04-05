45d10e77e1c921efae58b8dcbb39d06d482816ed
Igsu-Bravo <juan.bravo@futurice.com>
2021-04-05 13:43:34 +0300

    Define main routes

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "https://github.com/Igsu-Bravo/songs-clojure-api"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 ; Compojure - Routing
+                 [compojure "1.6.1"]
+                 ; HTTP lib for client/server
+                 [http-kit "2.3.0"]
+                 ; Ring defaults - query params
+                 [ring/ring-defaults "0.3.2"]
+                 ; Clojure data.JSON lib
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot songs-clojure-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/songs_clojure_api/core.clj
+++ src/songs_clojure_api/core.clj
@@ -1,7 +1,24 @@
 (ns songs-clojure-api.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as router]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as atr]
+            [clojure.data.json :as json])
   (:gen-class))
 
+(defroutes app-routes
+  (GET "/" [] simple-body-page)
+  (GET "/request" [] request-example)
+  (route/not-found "Page not found!"))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Perros y gatos"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run server without ring defaults
+    (server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http://127.0.0.1:" port "/"))))
